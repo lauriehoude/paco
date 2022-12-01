8c5ed8dd78733a19bd08e0b49c7d07df3a81b5c9
lauriehoude <2052728@cmontmorency.qc.ca>
2022-12-01 13:57:56 -0500

    5

--- chapitre1.js
+++ chapitre1.js
@@ -3,7 +3,9 @@ let chaptersObj = {
         subtitle: "Recommencer",
         text: " Vous avez pris la mauvaise décision, vous devez recommencer.",
         image: "assets/photo.jpg",
-        options: [{ text: "Recommencer!", action: "goToChapter('introduction')" }]
+        options: [{ text: "Recommencer!", action: "goToChapter('introduction')" },
+            { text: "Effacer ma partie", action: "reset()" }
+        ]
     },
 
     introduction: {
@@ -11,7 +13,9 @@ let chaptersObj = {
         text: "Paco le petit chat roux a entendu parlé d'un trésor caché celui de la forêt Valark.",
         image: "assets/OIP.jfif",
         video: "assets/videoplayback.mp4",
-        options: [{ text: "continuer", action: "goToChapter('introduction1')" }]
+        options: [{ text: "continuer", action: "goToChapter('introduction1')" },
+            { text: "Effacer ma partie", action: "reset()" }
+        ]
     },
 
     introduction1: {
@@ -23,7 +27,8 @@ let chaptersObj = {
             {
                 text: "Il reste dans son lit",
                 action: "goToChapter('lit_recommencer')"
-            }
+            },
+            { text: "Effacer ma partie", action: "reset()" }
 
         ]
     },
@@ -32,7 +37,9 @@ let chaptersObj = {
         subtitle: "Dormir recommencer",
         text: "Paco prefere dormir que de partir à l'aventure",
         image: "assets/cat_sleep.jpg",
-        options: [{ text: "dormir", action: "goToChapter('recommencer')" }]
+        options: [{ text: "dormir", action: "goToChapter('recommencer')" },
+            { text: "Effacer ma partie", action: "reset()" }
+        ]
     },
 
     sortir_dehors: {
@@ -41,7 +48,8 @@ let chaptersObj = {
         image: "assets/forest_2.jpg",
         options: [
             { text: "chemin gauche", action: "goToChapter('chemin_gauche_riviere')" },
-            { text: "chemin droit", action: "goToChapter('chemin_droit_montagne')" }
+            { text: "chemin droit", action: "goToChapter('chemin_droit_montagne')" },
+            { text: "Effacer ma partie", action: "reset()" }
         ]
     },
 
@@ -54,14 +62,17 @@ let chaptersObj = {
             {
                 text: "Trouver des matériaux",
                 action: "goToChapter('riviere_materiaux')"
-            }
+            },
+            { text: "Effacer ma partie", action: "reset()" }
         ]
     },
     nager: {
         subtitle: "Rivière",
         text: "Avec beaucoup de difficulté, il arrive à traverser la rivière.",
         image: "assets/chemin_riviere_2.png",
-        options: [{ text: "continuer", action: "goToChapter('riviere_materiaux')" }]
+        options: [{ text: "continuer", action: "goToChapter('riviere_materiaux')" },
+            { text: "Effacer ma partie", action: "reset()" }
+        ]
     },
 
 
@@ -69,7 +80,9 @@ let chaptersObj = {
         subtitle: " Forêt Valark ",
         text: "Paco marche, marche et marche ",
         image: "assets/chemin_riviere_2.png",
-        options: [{ text: "continuer", action: "goToChapter('finale')" }]
+        options: [{ text: "continuer", action: "goToChapter('finale')" },
+            { text: "Effacer ma partie", action: "reset()" }
+        ]
     },
 
     chemin_droit_montagne: {
@@ -81,7 +94,8 @@ let chaptersObj = {
             {
                 text: " Refuser son aide ",
                 action: "goToChapter('escalader_montagne')"
-            }
+            },
+            { text: "Effacer ma partie", action: "reset()" }
         ]
     },
 
@@ -89,14 +103,18 @@ let chaptersObj = {
         subtitle: " Potion recommencer",
         text: " Vous avez pris la décision d'accepter de l'aide ce qui vous fait échouer la quête. Vous devez recommencer. <br/> En prennant cette potion il commence à se sentir bizzare et tombe dans les pommes. Il a oublié qu'il faut jamais faire confiance aux sorcières des montagnes",
         image: "assets/potion.jpg",
-        options: [{ text: " recommencer ", action: "goToChapter('recommencer')" }]
+        options: [{ text: " recommencer ", action: "goToChapter('recommencer')" },
+            { text: "Effacer ma partie", action: "reset()" }
+        ]
     },
 
     escalader_montagne: {
         subtitle: "sommet de la montagne",
         text: "Il escade la montagne.",
         image: "assets/mountain_escalader.webp",
-        options: [{ text: "continuer", action: "aideStatus()" }],
+        options: [{ text: "continuer", action: "aideStatus()" },
+            { text: "Effacer ma partie", action: "reset()" }
+        ],
         video: "assets/chat.mp4"
     },
 
@@ -104,12 +122,24 @@ let chaptersObj = {
         subtitle: " La fin de la mission",
         text: "Après des heures et des heures de recherches, il trouve le fameux diamant dans la grotte. Cependant celui-ci est protégé par Layla. étant donné que Paco n'a demander aucune aide elle le lui donne!",
         image: "assets/forest_valark.jpg",
-        options: [{ text: "Recommencer", action: "goToChapter('introduction')" }]
+        options: [{ text: "Recommencer", action: "goToChapter('introduction')" },
+            { text: "Effacer ma partie", action: "reset()" }
+        ]
     }
 };
-let musique = lovebomb.play();
 
 function goToChapter(chapterName) {
+
+    let input = document.querySelector('[type="checkbox"]');
+    console.log(input)
+    let inputOui = input.checked;
+    console.log(inputOui)
+    if (inputOui == true) {
+        const lovebomb = new Audio("assets/lovebomb.mp3")
+        lovebomb.currentTime = 0;
+        lovebomb.play()
+    }
+
     console.log(chaptersObj[chapterName]["subtitle"]);
     console.log(chaptersObj[chapterName]["text"]);
     console.log(chaptersObj[chapterName]["image"]);
@@ -145,16 +175,8 @@ function goToChapter(chapterName) {
 
     };
     localStorage.setItem("chapter", [chapterName]);
-    let input = document.querySelector('[type="checkbox"]');
-    let inputOui = input.checked;
-    if (inputOui == true) {
-        let lovebomb = new Audio("assets/lovebomb.mp3")
-        lovebomb.currentTime = 0;
-        lovebomb.play()
 
-    } else {
 
-    }
 
 
 }
@@ -205,15 +227,10 @@ function startGame() {
 }
 startGame()
 
-let efface = document.querySelector(".effacer")
-efface.addEventListener("click", function() {
+function reset() {
+    localStorage.clear();
+    keyFound = false;
+    aideDemande = false;
+    goToChapter("introduction");
 
-    function reset() {
-        localStorage.clear();
-        keyStatus = false;
-        aideTrue = false;
-        aideStatus = false;
-
-    }
-    reset();
-})
+}
